88ecc3b555c66a4db7f1ca8d33e3974a6a0f2f89
KimCookieYa <min49590@gmail.com>
2023-12-13 20:12:07 +0900

    Feat: 공통 헤더 구현 중

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <>
+      <header className="flex w-full h-12 bg-slate-300">
+        <h3>AI Developers</h3>
+        <div className="flex justify-evenly">
+          <li>Study</li>
+          <li>About</li>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
