6f3157e65af3d426105408c37513229c42d3ae72
KimCookieYa <min49590@gmail.com>
2023-12-14 04:37:50 +0900

    Design: 시간지연 방식 대신 스크롤값과 opcity를 매핑

--- src/components/Main/MainSecond.tsx
+++ src/components/Main/MainSecond.tsx
@@ -1,12 +1,13 @@
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 
 function MainSecond() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <div className="flex items-center justify-center w-full h-screen font-bold text-center">
       <motion.div
         className="flex flex-col items-center justify-center w-full gap-12"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        style={{ opacity: scrollYProgress }}
       >
         <span className="text-5xl">부산대학교 인공지능 학회</span>
         <div className="w-1/3 h-1 bg-secondary" />
