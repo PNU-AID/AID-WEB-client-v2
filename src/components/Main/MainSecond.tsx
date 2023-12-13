aa997f71b22f773683adc1c6504609d22437770c
KimCookieYa <min49590@gmail.com>
2023-12-14 04:06:18 +0900

    Design: 메인페이지 두번째 컴포넌트 구현

--- src/components/Main/MainSecond.tsx
+++ src/components/Main/MainSecond.tsx
@@ -1,7 +1,11 @@
 function MainSecond() {
   return (
-    <div className="flex items-center justify-center w-full h-screen text-4xl font-bold text-center text-white">
-      second{' '}
+    <div className="flex items-center justify-center w-full h-screen font-bold text-center">
+      <div className="flex flex-col items-center justify-center w-full gap-12">
+        <span className="text-5xl">부산대학교 인공지능 학회</span>
+        <div className="w-1/3 h-1 bg-secondary" />
+        <span className="text-black text-7xl">AID</span>
+      </div>
     </div>
   );
 }
