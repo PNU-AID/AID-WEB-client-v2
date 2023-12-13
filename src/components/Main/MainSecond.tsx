7c05cc01116275d68d222123db66a14a956de92e
KimCookieYa <min49590@gmail.com>
2023-12-14 04:18:07 +0900

    Design: framer-motion 설치 && 스크롤시 나타나도록 변경

--- src/components/Main/MainSecond.tsx
+++ src/components/Main/MainSecond.tsx
@@ -1,11 +1,17 @@
+import { motion } from 'framer-motion';
+
 function MainSecond() {
   return (
     <div className="flex items-center justify-center w-full h-screen font-bold text-center">
-      <div className="flex flex-col items-center justify-center w-full gap-12">
+      <motion.div
+        className="flex flex-col items-center justify-center w-full gap-12"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
         <span className="text-5xl">부산대학교 인공지능 학회</span>
         <div className="w-1/3 h-1 bg-secondary" />
         <span className="text-black text-7xl">AID</span>
-      </div>
+      </motion.div>
     </div>
   );
 }
