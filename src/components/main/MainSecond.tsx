21dd36271d590f45eb9bcb2679396d96f925102c
KimCookieYa <min49590@gmail.com>
2023-12-15 04:36:19 +0900

    Fix: 파일 대소문자 추적하도록 수정

---
+++ src/components/main/MainSecond.tsx
@@ -0,0 +1,20 @@
+import { motion, useScroll } from 'framer-motion';
+
+function MainSecond() {
+  const { scrollYProgress } = useScroll({ offset: ['50vh', '100vh'] });
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen font-bold text-center">
+      <motion.div
+        className="flex flex-col items-center justify-center w-full gap-12"
+        style={{ opacity: scrollYProgress }}
+      >
+        <span className="text-5xl">부산대학교 인공지능 학회</span>
+        <div className="w-1/3 h-1 bg-secondary" />
+        <span className="text-black text-7xl">AID</span>
+      </motion.div>
+    </div>
+  );
+}
+
+export default MainSecond;
