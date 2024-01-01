e520dedc32953b4d73a4a5c72cad31b7893521f9
KimCookieYa <min49590@gmail.com>
2024-01-02 02:46:07 +0900

    Design: 로그인 페이지 디자인 구현  및 지원 페이지 디자인 수정

--- src/components/form/InputForm.tsx
+++ src/components/form/InputForm.tsx
@@ -1,22 +1,32 @@
 import { InputFormProps } from 'form';
+import { useEffect, useState } from 'react';
 
 function InputForm(data: InputFormProps) {
+  const [inputSize, setInputSize] = useState<string>('400px');
+  useEffect(() => {
+    if (!data.size || data.size === 'lg') {
+      setInputSize('500px');
+    }
+  }, [data.size]);
+
   return (
-    <div className="flex flex-col w-full h-full gap-4">
-      <label>{data.label}</label>
-      <input
-        className="w-[500px] h-10 bg-white rounded-md text-black px-4 border-[1px] border-black"
-        name={data.name}
-        onBlur={data.onBlur}
-        onChange={data.onChange}
-        required
-        style={{
-          outline: data.error ? '1px solid rgba(255, 0, 0, 0.8)' : 'none',
-        }}
-        type={data.type}
-        value={data.value}
-      />
-      {data.error && <p className="text-red-500">필수 입력 항목입니다.</p>}
+    <div className="flex justify-center">
+      <div className="flex flex-col h-full gap-3">
+        <label>{data.label}</label>
+        <input
+          className={`w-[350px] h-9 bg-white rounded-md text-black px-4 border-[1px] border-black`}
+          name={data.name}
+          onBlur={data.onBlur}
+          onChange={data.onChange}
+          required
+          style={{
+            outline: data.error ? '1px solid rgba(255, 0, 0, 0.8)' : 'none',
+          }}
+          type={data.type}
+          value={data.value}
+        />
+        {data.error && <p className="text-red-500">필수 입력 항목입니다.</p>}
+      </div>
     </div>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,23 @@
+import { Link } from 'react-router-dom';
+import Button from '../components/button/Button';
 import InputForm from '../components/form/InputForm';
 
 function LoginPage() {
   return (
     <section className="flex items-center justify-center w-full min-h-screen bg-primary">
-      <div className="flex flex-col items-center justify-around w-7/12 h-[50vh] p-4 bg-white">
-        <p className="text-3xl font-bold text-black">AID 로그인</p>
-        <form className="flex flex-col h-3/4" id="login-form">
+      <div className="flex flex-col items-center justify-around w-1/2 h-[55vh] p-4 bg-white shadow-2xl mt-10">
+        <p className="mb-8 text-3xl font-bold text-black">AID 로그인</p>
+        <form
+          className="flex flex-col items-center gap-4 h-1/2"
+          id="login-form"
+        >
           <InputForm
             error={false}
             label="이메일(Email)"
             name="email"
             onBlur={() => {}}
             onChange={() => {}}
+            size="md"
             type="text"
             value={''}
           />
@@ -21,11 +27,15 @@ function LoginPage() {
             name="password"
             onBlur={() => {}}
             onChange={() => {}}
+            size="md"
             type="text"
             value={''}
           />
-          <button>로그인</button>
         </form>
+        <Button label="로그인" primary />
+        <Link className="hover:text-primary" to="/register">
+          회원가입
+        </Link>
       </div>
     </section>
   );
