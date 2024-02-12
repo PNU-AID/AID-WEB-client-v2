7427994fd3791092b92a046d62b0606eb9532f7d
yeonddori <lsymiso@naver.com>
2024-02-12 14:49:40 +0900

    Feat: 스터디 검색 기능 구현

--- src/components/studypage/Search.tsx
+++ src/components/studypage/Search.tsx
@@ -1,21 +1,35 @@
 import { useState } from 'react';
-import { RiSearchLine } from 'react-icons/ri';
+import { RiSearchLine, RiCloseLine } from 'react-icons/ri';
 
-const Search = () => {
+const Search = ({ onSearch }: { onSearch: (searchText: string) => void }) => {
   const [inputValue, setInputValue] = useState('');
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setInputValue(value);
+    onSearch(value);
+  };
+
+  const handleClear = () => {
+    setInputValue('');
+    onSearch('');
+  };
+
   return (
-    <div className="flex items-center h-8 px-5 bg-gray-100 rounded-full ">
-      <RiSearchLine className="mr-2 text-gray-500" />
+    <div className="relative flex items-center h-8 bg-gray-100 rounded-full">
+      <RiSearchLine className="absolute text-gray-500 left-3 top-2" />
       <input
-        className="font-semibold bg-transparent outline-none"
-        onChange={(e) => {
-          const { value } = e.target;
-          setInputValue(value);
-        }}
-        placeholder="제목을 검색해보세요."
+        className="w-64 pl-10 font-semibold bg-transparent outline-none"
+        onChange={handleChange}
+        placeholder="제목이나 내용을 검색해보세요."
         value={inputValue}
       ></input>
+      {inputValue && (
+        <RiCloseLine
+          className="absolute text-gray-500 cursor-pointer right-3 top-2"
+          onClick={handleClear}
+        />
+      )}
     </div>
   );
 };
