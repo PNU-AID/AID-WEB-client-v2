8a20385290c3791b73fb8d50c48afb4af3fa0b99
yeonddori <lsymiso@naver.com>
2024-02-12 14:45:42 +0900

    Refactor: util 분리

--- src/data/study-data.ts
+++ src/data/study-data.ts
@@ -143,33 +143,3 @@ export const ProjectItemList: ProjectItem[] = [
     target: '파이썬에 능숙하신 분~~~~',
   },
 ];
-
-export const categoryData = [
-  {
-    id: 1,
-    name: '전체',
-  },
-  {
-    id: 2,
-    name: '프로젝트',
-  },
-  {
-    id: 3,
-    name: '스터디',
-  },
-];
-
-export const statusData = [
-  {
-    id: 1,
-    name: '모집 중',
-  },
-  {
-    id: 2,
-    name: '진행 중',
-  },
-  {
-    id: 3,
-    name: '진행 완료',
-  },
-];
---
+++ src/utils/study.ts
@@ -0,0 +1,11 @@
+export const categoryData = [
+  { id: 1, name: '전체' },
+  { id: 2, name: '스터디' },
+  { id: 3, name: '프로젝트' },
+];
+
+export const statusData = [
+  { id: 1, name: '모집 중' },
+  { id: 2, name: '진행 중' },
+  { id: 3, name: '진행 완료' },
+];
