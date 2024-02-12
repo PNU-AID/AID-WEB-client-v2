d53dcd30a7656af7edc08f2d6dcbaa35466b55f9
yeonddori <lsymiso@naver.com>
2024-02-12 14:59:50 +0900

    Feat: 스터디 페이지 구현

--- src/components/studypage/Posts.tsx
+++ src/components/studypage/Posts.tsx
@@ -1,59 +1,103 @@
-import { useState, ChangeEvent } from 'react';
-import { StudyItem } from '../../type/study';
-import { StudyItemList, categoryData, onOffline } from '../../data/study-data';
-import Select from './Select';
+import { useEffect, useState } from 'react';
+import { categoryData, statusData } from '@util/study';
+import { ProjectItem, StudyItem } from '../../types/study';
+import { ProjectItemList, StudyItemList } from '../../data/study-data';
 import Search from './Search';
 import Modal from './Modal';
 
+type CombinedItem = StudyItem | ProjectItem;
+
 function Posts() {
-  const [study, setStudy] = useState<StudyItem[]>(StudyItemList);
-  const [status, setStatus] = useState<boolean>(false);
+  const [items, setItems] = useState<CombinedItem[]>([
+    ...StudyItemList,
+    ...ProjectItemList,
+  ]);
+  const [status, setStatus] = useState<string>();
   const [category, setCategory] = useState<string>('전체');
-  const [selectedStudy, setSelectedStudy] = useState<StudyItem | null>(null);
-  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [selectedItem, setSelectedItem] = useState<CombinedItem | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [searchText, setSearchText] = useState<string>('');
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   const filterCategory = (selectedCategory: string) => {
-    setStatus(false);
+    setStatus('');
     setCategory(selectedCategory);
     if (selectedCategory === '전체') {
-      setStudy(StudyItemList);
+      setItems([...StudyItemList, ...ProjectItemList]);
+    } else if (selectedCategory === '프로젝트') {
+      setItems(ProjectItemList);
     } else {
-      setStudy(
-        StudyItemList.filter((item) => {
-          return item.category === selectedCategory;
-        })
-      );
+      setItems(StudyItemList);
     }
   };
 
-  const filterStatus = () => {
-    setStatus((prevStatus) => {
-      const newStatus = !prevStatus;
-      if (newStatus) {
-        setStudy((prevStudy) =>
-          prevStudy.filter((item) => item.status === '모집 중')
-        );
-      } else {
-        filterCategory(category);
-      }
-      return newStatus;
-    });
+  const filterStatus = (selectedStatus: string) => {
+    setStatus((prevStatus) =>
+      prevStatus === selectedStatus ? '' : selectedStatus
+    );
+    let filteredItems =
+      category === '프로젝트'
+        ? ProjectItemList
+        : category === '스터디'
+          ? StudyItemList
+          : [...StudyItemList, ...ProjectItemList];
+
+    if (status !== selectedStatus) {
+      filteredItems = filteredItems.filter(
+        (item) => item.status === selectedStatus
+      );
+    }
+    setItems(filteredItems);
   };
 
-  const filterOnOffline = (e: ChangeEvent<HTMLSelectElement>) => {
-    const { value } = e.target;
-    setSelectedOption(value);
-  };
-
-  const handleStudyClick = (selectedStudy: StudyItem) => {
-    setSelectedStudy(selectedStudy);
+  const handleItemClick = (selectedItem: CombinedItem) => {
+    setSelectedItem(selectedItem);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  const applyFilters = (itemsToFilter: CombinedItem[]) => {
+    return itemsToFilter.filter((item) => {
+      return (
+        ('studyName' in item &&
+          item.studyName &&
+          item.studyName.toLowerCase().includes(searchText.toLowerCase())) ||
+        ('projectName' in item &&
+          item.projectName &&
+          item.projectName.toLowerCase().includes(searchText.toLowerCase())) ||
+        ('studyDescription' in item &&
+          item.studyDescription &&
+          item.studyDescription
+            .toLowerCase()
+            .includes(searchText.toLowerCase())) ||
+        ('projectDescription' in item &&
+          item.projectDescription &&
+          item.projectDescription
+            .toLowerCase()
+            .includes(searchText.toLowerCase()))
+      );
+    });
+  };
+
+  const filteredItems = searchText ? applyFilters(items) : items;
 
   return (
     <>
@@ -71,51 +115,53 @@ function Posts() {
         ))}
       </section>
       <section className="flex items-center justify-between">
-        <div className="flex gap-3">
-          <Select
-            onChange={filterOnOffline}
-            options={onOffline}
-            placeholder={'진행 방식'}
-            value={selectedOption}
-          />
-          <div
-            className={`flex h-10 px-6 justify-center items-center rounded-full border ${
-              status ? 'border-blue-500' : 'border-gray-300'
-            } ${
-              status ? 'text-blue-500' : 'text-gray-600'
-            } bg-white font-medium cursor-pointer`}
-            onClick={() => filterStatus()}
-          >
-            모집 중만 보기
-          </div>
-        </div>
-        <Search />
+        <section className="flex gap-3">
+          {statusData.map((item, index) => (
+            <div
+              className={`flex h-10 px-6 justify-center items-center rounded-full border ${
+                status === item.name
+                  ? 'border-blue-500 text-blue-500'
+                  : 'border-gray-300 text-gray-600'
+              } bg-white font-medium cursor-pointer`}
+              key={index}
+              onClick={() => filterStatus(item.name)}
+            >
+              {item.name}만 보기
+            </div>
+          ))}
+        </section>
+        <Search onSearch={handleSearch} />
       </section>
       <section>
         <div className="grid grid-cols-2 gap-6 lg:grid-cols-4">
-          {study.map((item, index) => (
+          {filteredItems.map((item, index) => (
             <div
-              className="flex flex-col p-4 bg-white border-2 border-gray-300 rounded-2xl hover:shadow-md"
+              className="flex flex-col p-4 bg-white border-2 border-gray-300 rounded-2xl hover:shadow-lg hover:cursor-pointer"
               key={index}
-              onClick={() => handleStudyClick(item)}
+              onClick={() => handleItemClick(item)}
             >
               <div className="flex justify-between mb-3 text-sm font-bold text-gray-400 border-b-2">
-                <span>{item.category}</span>
+                <span>{'studyName' in item ? '스터디' : '프로젝트'}</span>
                 <span>~{item.date}</span>
               </div>
-              <p className="text-xl font-bold text-gray-800">
-                {item.studyName}
-              </p>
-              <p className="mt-2 overflow-hidden text-sm text-gray-600 h-36">
-                {item.studyDescription}
-              </p>
-              <div className="flex items-center mt-3 text-sm text-gray-700">
+              <div className="flex items-center justify-center h-40 mb-5">
                 <img
-                  alt={item.leader}
-                  className="w-6 h-6 mr-2 rounded-full"
+                  alt="Thumbnail"
+                  className="object-cover w-full h-full rounded"
                   src={item.imgUrl}
                 />
-                {item.leader}
+              </div>
+              <div className="ml-3 mr-3">
+                <p className="text-xl font-bold text-gray-800">
+                  {'studyName' in item
+                    ? (item as StudyItem).studyName
+                    : (item as ProjectItem).projectName}
+                </p>
+                <p className="h-20 mt-2 overflow-hidden text-sm text-gray-600">
+                  {'studyDescription' in item
+                    ? (item as StudyItem).studyDescription
+                    : (item as ProjectItem).projectDescription}
+                </p>
               </div>
             </div>
           ))}
@@ -124,7 +170,7 @@ function Posts() {
       <Modal
         isOpen={isModalOpen}
         onClose={closeModal}
-        selectedStudy={selectedStudy}
+        selectedItem={selectedItem}
       ></Modal>
     </>
   );
