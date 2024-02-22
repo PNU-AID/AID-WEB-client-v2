d1b00acb0e73a78af27fffe904408b9324f9c2ec
yeonddori <lsymiso@naver.com>
2024-02-23 01:43:07 +0900

    Design: 헤더 및 언어 선택 토글 디자인 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -67,161 +67,167 @@ function Header() {
     >
       <div className="flex items-center justify-around w-full ">
         <div className="flex items-center">
-          <Link className="relative m-2" to="/">
-            <img
-              alt="aid-logo"
-              className="absolute top-0 bottom-0 m-auto"
-              src={AidLogoImg}
-              width={20}
-            />
-            <h3
-              className={[
-                'flex justify-start ml-8 text-2xl font-bold',
-                'transition-all duration-300',
-                !isDarkPage(location.pathname)
-                  ? 'text-black'
-                  : lastScrollY > 0 || isExpanded
-                    ? textCss
-                    : 'text-black',
-              ].join(' ')}
-              id="aid-logo"
-            >
-              AI Developer
-            </h3>
-          </Link>
-          <LanguageToggle />
-        </div>
-        <MenuIconSvg
-          className="sm:hidden"
-          onClick={() => setIsExpanded(!isExpanded)}
-          width={70}
-        />
-        <nav
-          className={[
-            'flex flex-col absolute top-[60px] gap-0 sm:mb-4 w-full shadow-xl font-sans text-center rounded-xl pb-2',
-            'sm:justify-end sm:w-fit sm:gap-0 sm:pt-2 sm:flex-row sm:shadow-none sm:static sm:mx-8 sm:my-2 sm:text-left sm:pb-0 sm:rounded-none',
-            'transition-all duration-500',
-            isExpanded
-              ? 'max-h-[500px] bg-white'
-              : 'max-h-0 bg-transparent sm:max-h-[300px]',
-          ].join(' ')}
-        >
-          <a href={import.meta.env.VITE_AID_NOTION_NEWS_URL} target="_blank">
-            <Button
-              className={[
-                !isDarkPage(location.pathname)
-                  ? 'text-black'
-                  : lastScrollY > 0 || isExpanded
-                    ? textCss
-                    : 'text-black',
-              ].join(' ')}
-              label="About Us"
-              size="large"
-            />
-          </a>
-          {headerData.map((item) => {
-            if (item.label === 'Login') {
-              if (authState.isAuthenticated) {
-                return (
-                  <div className="relative">
-                    <button
-                      className="px-4 py-3 rounded-lg hover:bg-gray-100"
-                      onClick={toggleProfile}
-                    >
-                      <span className="text-primary">
-                        {authState.authInfo!.nickname}
-                      </span>
-                      님, 안녕하세요!
-                    </button>
-                    <HeaderProfile
-                      isExpanded={isVisible && isProfileExpanded}
+          <div className="flex items-center">
+            <Link className="relative m-2" to="/">
+              <img
+                alt="aid-logo"
+                className="absolute top-0 bottom-0 m-auto"
+                src={AidLogoImg}
+                width={20}
+              />
+              <h3
+                className={[
+                  'flex justify-start ml-8 text-2xl font-bold',
+                  'transition-all duration-300',
+                  !isDarkPage(location.pathname)
+                    ? 'text-black'
+                    : lastScrollY > 0 || isExpanded
+                      ? textCss
+                      : 'text-black',
+                ].join(' ')}
+                id="aid-logo"
+              >
+                AI Developer
+              </h3>
+            </Link>
+          </div>
+          <MenuIconSvg
+            className="sm:hidden"
+            onClick={() => setIsExpanded(!isExpanded)}
+            width={70}
+          />
+          <nav
+            className={[
+              'flex flex-col absolute top-[60px] gap-0 sm:mb-4 w-full shadow-xl font-sans text-center rounded-xl pb-2',
+              'sm:justify-end sm:w-fit sm:gap-0 sm:pt-2 sm:flex-row sm:shadow-none sm:static sm:mx-8 sm:my-2 sm:text-left sm:pb-0 sm:rounded-none',
+              'transition-all duration-500',
+              isExpanded
+                ? 'max-h-[500px] bg-white'
+                : 'max-h-0 bg-transparent sm:max-h-[300px]',
+            ].join(' ')}
+          >
+            <a href={import.meta.env.VITE_AID_NOTION_NEWS_URL} target="_blank">
+              <Button
+                className={[
+                  !isDarkPage(location.pathname)
+                    ? 'text-black'
+                    : lastScrollY > 0 || isExpanded
+                      ? textCss
+                      : 'text-black',
+                ].join(' ')}
+                label="About Us"
+                size="large"
+              />
+            </a>
+            {headerData.map((item) => {
+              if (item.label === 'Login') {
+                if (authState.isAuthenticated) {
+                  return (
+                    <div className="relative">
+                      <button
+                        className="px-4 py-3 rounded-lg hover:bg-gray-100"
+                        onClick={toggleProfile}
+                      >
+                        <span className="text-primary">
+                          {authState.authInfo!.nickname}
+                        </span>
+                        님, 안녕하세요!
+                      </button>
+                      <HeaderProfile
+                        isExpanded={isVisible && isProfileExpanded}
+                      />
+                    </div>
+                  );
+                } else {
+                  <Link to="/login">
+                    <Button
+                      className={[
+                        location.pathname.slice(1) === 'login'
+                          ? 'font-bold'
+                          : '',
+                        !isDarkPage(location.pathname)
+                          ? 'text-black'
+                          : lastScrollY > 0 || isExpanded
+                            ? textCss
+                            : 'text-black',
+                      ].join(' ')}
+                      label="Login"
+                      size="large"
                     />
-                  </div>
-                );
-              } else {
-                <Link to="/login">
+                  </Link>;
+                }
+              }
+              return (
+                <Link key={item.label} to={item.path}>
                   <Button
                     className={[
-                      location.pathname.slice(1) === 'login' ? 'font-bold' : '',
+                      location.pathname.slice(1) === item.path
+                        ? 'font-bold'
+                        : '',
                       !isDarkPage(location.pathname)
                         ? 'text-black'
                         : lastScrollY > 0 || isExpanded
                           ? textCss
                           : 'text-black',
                     ].join(' ')}
-                    label="Login"
+                    label={item.label}
                     size="large"
                   />
-                </Link>;
-              }
-            }
-            return (
-              <Link key={item.label} to={item.path}>
-                <Button
+                </Link>
+              );
+            })}
+            <div className="relative flex flex-col items-center justify-center">
+              <div
+                className="flex px-6 py-3 cursor-pointer"
+                onClick={() => setIsDropdownExpanded((prev) => !prev)}
+              >
+                <button
                   className={[
-                    location.pathname.slice(1) === item.path ? 'font-bold' : '',
                     !isDarkPage(location.pathname)
                       ? 'text-black'
                       : lastScrollY > 0 || isExpanded
                         ? textCss
                         : 'text-black',
                   ].join(' ')}
-                  label={item.label}
-                  size="large"
+                >
+                  Contack Us
+                </button>
+                <IoIosArrowDown
+                  className={[
+                    !isDarkPage(location.pathname)
+                      ? 'text-black'
+                      : lastScrollY > 0 || isExpanded
+                        ? textCss
+                        : 'text-black',
+                  ].join(' ')}
                 />
-              </Link>
-            );
-          })}
-          <div className="relative flex flex-col items-center justify-center">
-            <div
-              className="flex px-6 py-3 cursor-pointer"
-              onClick={() => setIsDropdownExpanded((prev) => !prev)}
-            >
-              <button
-                className={[
-                  !isDarkPage(location.pathname)
-                    ? 'text-black'
-                    : lastScrollY > 0 || isExpanded
-                      ? textCss
-                      : 'text-black',
-                ].join(' ')}
-              >
-                Contack Us
-              </button>
-              <IoIosArrowDown
-                className={[
-                  !isDarkPage(location.pathname)
-                    ? 'text-black'
-                    : lastScrollY > 0 || isExpanded
-                      ? textCss
-                      : 'text-black',
-                ].join(' ')}
-              />
-            </div>
-            {
-              <div
-                className={[
-                  'absolute w-fit top-[60px] bg-white rounded-lg overflow-hidden duration-500 ease',
-                  isDropdownExpanded ? 'max-h-screen shadow-xl' : 'max-h-0',
-                ].join(' ')}
-              >
-                {aidUrls.map((item) => {
-                  return (
-                    <a
-                      className="flex items-center px-4 py-2 text-sm text-black cursor-pointer gap-x-1"
-                      href={item.url}
-                      key={item.label}
-                      target="_blank"
-                    >
-                      <item.image />
-                      <span>{item.label}</span>
-                    </a>
-                  );
-                })}
               </div>
-            }
-          </div>
-        </nav>
+              {
+                <div
+                  className={[
+                    'absolute w-fit top-[60px] bg-white rounded-lg overflow-hidden duration-500 ease',
+                    isDropdownExpanded ? 'max-h-screen shadow-xl' : 'max-h-0',
+                  ].join(' ')}
+                >
+                  {aidUrls.map((item) => {
+                    return (
+                      <a
+                        className="flex items-center px-4 py-2 text-sm text-black cursor-pointer gap-x-1"
+                        href={item.url}
+                        key={item.label}
+                        target="_blank"
+                      >
+                        <item.image />
+                        <span>{item.label}</span>
+                      </a>
+                    );
+                  })}
+                </div>
+              }
+            </div>
+          </nav>
+        </div>{' '}
+        <LanguageToggle />
       </div>
     </header>
   );
